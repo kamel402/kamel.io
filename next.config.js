e652717761a665de3b5cad9659fba214215084ae
kamel402 <kamel1419@gmail.com>
2023-02-22 04:51:38 +0300

    modfiy assetPrefix & basePath

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+const repo = 'kamel.io'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
 
 module.exports = {
     assetPrefix: assetPrefix,
