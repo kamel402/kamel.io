528cb92a43105662972cdca04adff9436f960d69
kamel402 <kamel1419@gmail.com>
2023-02-22 05:46:45 +0300

    add repo url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-const repo = 'https://kamel402.github.io/kamel.io'
-const assetPrefix = `/${repo}`
-const basePath = `/${repo}`
+// next.config.js
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 module.exports = {
     assetPrefix: assetPrefix,
